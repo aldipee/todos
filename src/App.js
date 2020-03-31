866885fd9163e964c61d3df4fef59af49d391345
aldipee <aldipeee@gmail.com>
2020-03-31 09:46:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Row, Container, Col, Input as BsInput, Button, Card as BsCard } from 'reactstrap'
+import styled from 'styled-components'
+import { MdCheckCircle } from 'react-icons/md'
+
+const Header = styled('div')`
+  display: flex;
+  height: 60px;
+  background-color: #2db495;
+  flex-direction: column;
+  padding: 10px;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
+`
+
+const Input = styled(BsInput)`
+  background-color: rgba(255, 255, 255, 0.3);
+  border: none;
+  &:focus {
+    background-color: rgba(255, 255, 255, 0.3);
+    color: #fff;
+    border: none;
+  }
+  &&::placeholder {
+    color: #fff;
+  }
+`
+
+const Body = styled('div')`
+  background-color: #7de0e0;
+  padding: 10px;
+  min-height: 500px;
+`
+const Card = styled(BsCard)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+  color: #777;
+  cursor: pointer;
+`
+
+export default class App extends Component {
+  state = {
+    todos: []
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Row>
+            <Col md={12}>
+              <Input placeholder="Enter your today tasks!" type="text" />
+            </Col>
+          </Row>
+        </Header>
+        <Body>
+          <Row>
+            {this.state.todos.length &&
+              this.state.todos.map((data, i) => (
+                <Col md={12}>
+                  <Card>
+                    <span>Todo 1</span>
+                    <span>
+                      <MdCheckCircle color="#35d660" size={25} />
+                    </span>
+                  </Card>
+                </Col>
+              ))}
+          </Row>
+        </Body>
+      </Container>
+    )
+  }
 }
-
-export default App;
